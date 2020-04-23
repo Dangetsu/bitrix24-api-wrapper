d2552dc3715393105a82263c721bad107aeaa8bf
Vladislav Alatorcev <clannad_forever@mail.ru>
2020-04-23 15:59:33 +0300

    Own exceptions

--- src/Engine/AbstractBasic.php
+++ src/Engine/AbstractBasic.php
@@ -9,6 +9,9 @@ abstract class AbstractBasic {
     private const METHOD_GET  = 'GET';
     private const METHOD_POST = 'POST';
 
+    private const HTTP_CODE_ACCESS_DENIED = 403;
+    private const HTTP_CODE_NOT_FOUND     = 404;
+
     abstract protected function _prepareUrl(string $apiMethod): string;
 
     /**
@@ -40,7 +43,22 @@ abstract class AbstractBasic {
      * @return mixed
      */
     private function _request(string $httpMethod, string $apiMethod, array $parameters = []) {
-        $res = $this->_httpClient()->request($httpMethod, $this->_prepareUrl($apiMethod), $parameters);
-        return $res->getBody();
+        try {
+            $res = $this->_httpClient()->request($httpMethod, $this->_prepareUrl($apiMethod), $parameters);
+            return $res->getBody();
+        } catch (GuzzleHttp\Exception\RequestException $exception) {
+            throw $this->_replaceRequestException($exception);
+        }
+    }
+
+    private function _replaceRequestException(GuzzleHttp\Exception\RequestException $exception): Exception\Basic {
+        switch ($exception->getCode()) {
+            case self::HTTP_CODE_ACCESS_DENIED:
+                return new Exception\AccessDenied($exception->getMessage());
+            case self::HTTP_CODE_NOT_FOUND:
+                return new Exception\NotFound($exception->getMessage());
+            default:
+                return new Exception\Basic($exception->getMessage());
+        }
     }
 }
--- test/Engine/BasicInterfaceTest.php
+++ test/Engine/BasicInterfaceTest.php
@@ -3,13 +3,12 @@
 namespace Bitrix24ApiWrapper\Test\Engine;
 
 use Bitrix24ApiWrapper\Engine;
-use GuzzleHttp\Exception\RequestException;
 
 class BasicInterfaceTest extends \PHPUnit\Framework\TestCase {
 
     public function testGet() {
         $engine = new Engine\WebHook('https://b24-xxxxxxx.bitrix24.ru/rest/1/******');
-        $this->expectException(RequestException::class);
+        $this->expectException(Engine\Exception\AccessDenied::class);
         $engine->get('test');
     }
 }
